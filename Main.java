8ce14aa48d1bbcb260ddce1e09b90a2281eaca7c
kczerwonka00 <101180432+kczerwonka00@users.noreply.github.com>
2022-03-22 10:04:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+class Main {
+  public static void main(String[] args) {
+    int liczba = 2;
+    double a = 4;
+    char b = 'd';
+    byte c = 8;
+
+    System.out.println("int =" +liczba);
+    System.out.println("double =" +a);
+    System.out.println("char =" +b);
+    System.out.println("byte =" +c);
+    
+    
+    
+  }
+}
